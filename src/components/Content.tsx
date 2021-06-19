35f8bbcc0e37b27c808ead4eeca7dc21dcbcf679
Gabriel Coelho <gaaucocoelho@gmail.com>
2021-06-19 19:13:08 -0300

    refactor: create Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+import "../styles/content.scss";
+
+import IMovie from "../dtos/MovieDTO";
+
+import { MovieCard } from "./MovieCard";
+import { Header } from "./Header";
+
+interface ContentProps {
+  movies: IMovie[];
+  headerTitle: string;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <Header title={props.headerTitle} />
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
