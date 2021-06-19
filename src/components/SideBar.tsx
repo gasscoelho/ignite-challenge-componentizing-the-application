90c11c9e3298c3024d507ee85b145dfd62e64370
Gabriel Coelho <gaaucocoelho@gmail.com>
2021-06-19 19:12:55 -0300

    refactor: create SideBar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,41 @@
-export function SideBar() {
-  // Complete aqui
+import "../styles/sidebar.scss";
+
+import { useCallback, useState } from "react";
+
+import IGenre from "../dtos/GenreDTO";
+
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: IGenre[];
+  onSelectedGenre(id: number): void;
+}
+
+export function SideBar(props: SideBarProps) {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  const handleClickButton = useCallback((id: number) => {
+    setSelectedGenreId(id);
+    props.onSelectedGenre(id);
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
